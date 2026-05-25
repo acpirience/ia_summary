0fd54f9ca4bb00f62875aeb974ad9ffe068a8510
Acpirience <acpirience@gmail.com>
2026-05-25 12:17:14 +0200

    create database

--- database.py
+++ database.py
@@ -9,10 +9,19 @@ class Database:
     def __init__(self):
         self.db_name: str = DB_NAME
         self.connection: sql.Connection | None = None
+        self.cursor: sql.Cursor | None = None
 
     def connect(self):
         self.connection = sql.connect(self.db_name)
-        logger.info(f"Connected to {self.db_name}")
+        if self.connection:
+            logger.info(f"Connected to {self.db_name}")
+        else:
+            logger.error(f"Failed to connect to {self.db_name}")
+            return
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS mails (title TEXT, id TEXT, date DATETIME, PRIMARY KEY (title, id))"
+        )
 
     def disconnect(self):
         if self.connection:
