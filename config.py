f41bc74c06bc37c2aa20e8fe3a8bbb2f33dfce95
Acpirience <acpirience@gmail.com>
2026-06-02 12:10:23 +0200

    add newline

--- config.py
+++ config.py
@@ -30,6 +30,7 @@ FROM_ADDRESS: list[dict[str, str]] = [
     {"title": "The Rundown AI", "email": "therundown.ai"},  # news@daily.therundown.ai
     {"title": "Anthyme De Minutora", "email": "laminutora"},  # laminutora@10812296.brevosend.com
     {"title": "Napkin AI", "email": "contact@napkin.ai"},  # contact@napkin.ai
+    {"title": "newline team", "email": "us@fullstack.io"},  # us@fullstack.io
 ]
 
 DAYS_AGO = 1  # Look back period
