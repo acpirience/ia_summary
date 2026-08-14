9a902536de4a80129a36172c128b6f02d9950e9b
Acpirience <acpirience@gmail.com>
2026-08-14 12:36:00 +0200

    add list

--- config.py
+++ config.py
@@ -29,6 +29,7 @@ FROM_ADDRESS: list[dict[str, str]] = [
     {"title": "AINews", "email": "ainews@substack.com"},  # swyx+ainews@substack.com
     {"title": "Emergent", "email": "emergent.sh"},  # team@ship.emergent.sh
     {"title": "Deeplearning Ai", "email": "deeplearning.ai"},  # hello@deeplearning.ai
+    {"title": "Bens Bites", "email": "bensbites@substack.com"},  # bensbites@substack.com
     # tech
     {"title": "The Rundown Tech", "email": "technews.therundown.ai"},  # crew@technews.therundown.ai
     {"title": "TLDR", "email": "tldrnewsletter.com"},  # dan@tldrnewsletter.com
